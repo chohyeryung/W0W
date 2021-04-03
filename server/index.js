a49aa5c8acf8c8753770423eeac77a9770c70019
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-04-03 15:24:11 +0900

    mongoDB connect success

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello world!'))
 
 app.listen(port, () => console.log(`W0W app is litening on ${port}`))
