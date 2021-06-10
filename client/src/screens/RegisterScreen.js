e6a57ab370541613d43c1decfce86744d94bb5bd
yiseungmin <s2019w33@e-mirim.hs.kr>
2021-06-10 10:47:27 +0900

    registerScreen, signinScreen design finish

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -119,102 +119,104 @@ class RegisterScreen extends React.Component {
         const emailInputRef = this.emailInputRef;
 
         return (
-            <View style={styles.container}>
-            {/* header */}
-            <View style={styles.header}>
-                <Text style={styles.sub_text}>Create your</Text>
-                <Text style={styles.main_text}>Account</Text>
-            </View>
-            {/* footer */}
-            <View style={{ flex: 2, marginTop: 100 }}>
-                {/* footer - input box */}
-                <View>
-                    <Text style={[styles.text_title, {
-                        marginTop: 60}]}>Name</Text>
-                    <TextInput
-                        style={[styles.text_input, {marginTop: 10}]}
-                        placeholder="Name"
-                        placeholderTextColor="#C4C4C4"
-                        autoCapitalize="none"
-                        onChangeText={(userName) => this.setUserName(userName)}
-                        onSubmitEditing={() =>
-                            emailInputRef.current && emailInputRef.current.focus()
-                        }
-                        keyboardType="default"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                        />
-                    <Text style={[styles.text_title, {
-                        marginTop: 60}]}>E-mail</Text>
-                    <TextInput
-                        style={[styles.text_input, {marginTop: 10}]}
-                        placeholder="Email"
-                        placeholderTextColor="#C4C4C4"
-                        autoCapitalize="none"
-                        ref={emailInputRef}
-                        onChangeText={(userEmail) => this.setUserEmail(userEmail)}
-                        onSubmitEditing={() =>
-                            passwordInputRef.current && passwordInputRef.current.focus()
-                        }
-                        keyboardType="email-address"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                        />
-                    <Text style={[styles.text_title, {
-                        marginTop: 50}]}>Password</Text>
-                    <TextInput
-                        style={[styles.text_input, {marginTop: 10}]}
-                        placeholder="Password"
-                        placeholderTextColor="#C4C4C4"
-                        autoCapitalize="none"
-                        secureTextEntry={true}
-                        ref={passwordInputRef}
-                        keyboardType="default"
-                        returnKeyType="next"
-                        onChangeText={(userPassword) => this.setUserPassword(userPassword)}
-                        onSubmitEditing={Keyboard.dismiss}
-                        blurOnSubmit={false}
-                    />
-                    {errorText != '' ? (
-                        <Text style={{marginTop:10, color:'#ff4d4d'}}>{errorText}</Text>
-                    ) : null }
-                    {/* checkbox */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+                <View style={styles.container}>
+                    <ScrollView>
+                        {/* header */}
+                    <View style={styles.header}>
+                        <Text style={styles.sub_text}>Create your</Text>
+                        <Text style={styles.main_text}>Account</Text>
+                    </View>
+                    {/* footer */}
+                    <View style={{ flex: 2 }}>
+                        {/* footer - input box */}
                         <View>
-                            <TouchableOpacity onPressOut={this.onChecked}>
-                            {checked ? (
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Icon name="checkbox" size={22} color="#35C9C9"></Icon>
-                                    <Text style={{ marginLeft: 5, fontSize: 15, }}>개인정보 동의</Text>
+                            <Text style={[styles.text_title, {
+                                marginTop: 60}]}>Name</Text>
+                            <TextInput
+                                style={[styles.text_input, {marginTop: 10}]}
+                                placeholder="Name"
+                                placeholderTextColor="#C4C4C4"
+                                autoCapitalize="none"
+                                onChangeText={(userName) => this.setUserName(userName)}
+                                onSubmitEditing={() =>
+                                    emailInputRef.current && emailInputRef.current.focus()
+                                }
+                                keyboardType="default"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                                />
+                            <Text style={[styles.text_title, {
+                                marginTop: 60}]}>E-mail</Text>
+                            <TextInput
+                                style={[styles.text_input, {marginTop: 10}]}
+                                placeholder="Email"
+                                placeholderTextColor="#C4C4C4"
+                                autoCapitalize="none"
+                                ref={emailInputRef}
+                                onChangeText={(userEmail) => this.setUserEmail(userEmail)}
+                                onSubmitEditing={() =>
+                                    passwordInputRef.current && passwordInputRef.current.focus()
+                                }
+                                keyboardType="email-address"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                                />
+                            <Text style={[styles.text_title, {
+                                marginTop: 50}]}>Password</Text>
+                            <TextInput
+                                style={[styles.text_input, {marginTop: 10}]}
+                                placeholder="Password"
+                                placeholderTextColor="#C4C4C4"
+                                autoCapitalize="none"
+                                secureTextEntry={true}
+                                ref={passwordInputRef}
+                                keyboardType="default"
+                                returnKeyType="next"
+                                onChangeText={(userPassword) => this.setUserPassword(userPassword)}
+                                onSubmitEditing={Keyboard.dismiss}
+                                blurOnSubmit={false}
+                            />
+                            {errorText != '' ? (
+                                <Text style={{marginTop:10, color:'#ff4d4d'}}>{errorText}</Text>
+                            ) : null }
+                            {/* checkbox */}
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, marginBottom: 80 }}>
+                                <View>
+                                    <TouchableOpacity onPressOut={this.onChecked}>
+                                    {checked ? (
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <Icon name="checkbox" size={22} color="#35C9C9"></Icon>
+                                            <Text style={{ marginLeft: 5, fontSize: 15, }}>개인정보 동의</Text>
+                                        </View>
+                                    ) : (
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <Icon name="checkbox-outline" size={22} color="#35C9C9" />
+                                            <Text style={{ marginLeft: 5, fontSize: 15 }}>개인정보 동의</Text>
+                                        </View>
+                                    )}
+                                    </TouchableOpacity>
                                 </View>
-                            ) : (
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Icon name="checkbox-outline" size={22} color="#35C9C9" />
-                                    <Text style={{ marginLeft: 5, fontSize: 15 }}>개인정보 동의</Text>
-                                </View>
-                            )}
-                            </TouchableOpacity>
+                                <View><Text style={{ color: '#35C9C9', fontSize: 15 }}>자세히 보기</Text></View>
+                            </View>
                         </View>
-                        <View><Text style={{ color: '#35C9C9', fontSize: 15 }}>자세히 보기</Text></View>
-                    </View>
+                        </View>
+                        <View style={{ flex: 3, justifyContent: 'flex-end' }}>
+                            {/* sign in button */}
+                            <TouchableOpacity style={[styles.login_btn]} onPress={this.handleSubmitPress}>
+                                <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold', marginTop: 18, }}>Sign Up</Text>
+                            </TouchableOpacity>
+                            {/* side menu */}
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, paddingHorizontal: 150 }}>
+                            <View><Text style={{ fontSize: 15 }}>Already have a account?</Text></View>
+                                <Text
+                                    style={{ fontSize: 15, color: '#35C9C9' }}
+                                    onPress={() => this.props.navigation.navigate('SignIn')}>
+                                    Log In
+                                </Text>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
-                </View>
-                <View style={{ flex: 3, marginTop: 150 }}>
-                    {/* sign in button */}
-                    <TouchableOpacity style={[styles.login_btn]} onPress={this.handleSubmitPress}>
-                        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold', marginTop: 18, }}>Sign Up</Text>
-                    </TouchableOpacity>
-                    {/* side menu */}
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, paddingHorizontal: 150 }}>
-                    <View><Text style={{ fontSize: 15 }}>Already have a account?</Text></View>
-                        <Text
-                            style={{ fontSize: 15, color: '#35C9C9' }}
-                            onPress={() => this.props.navigation.navigate('SignIn')}>
-                            Log In
-                        </Text>
-                    </View>
-                </View>
-        </View>
         )
     }
 }
