72d9f4893569f0a3930c6be7b2e8324f56bbfa07
yiseungmin <s2019w33@e-mirim.hs.kr>
2021-06-14 16:12:47 +0900

    forgot password screen added

--- client/src/screens/SignInScreen.js
+++ client/src/screens/SignInScreen.js
@@ -92,7 +92,7 @@ class SignInScreen extends React.Component {
         const request = axios({
             method: 'post',
             data: body,
-            url: 'http://563cefae3f78.ngrok.io/users/login',
+            url: 'http://ec2-34-227-38-106.compute-1.amazonaws.com/users/login',
             changeOrigin: true,
         }).then((response) =>{
             AsyncStorage.setItem(
---
+++ client/src/styles/ForgotPassStyles.js
@@ -0,0 +1,58 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const mainColor = "#35C9C9";
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        paddingHorizontal: 70,
+        paddingVertical: 70,
+        fontFamily: 'Noto Sans KR',
+        justifyContent: 'space-between',
+    },
+    header: {
+        flex: 1,
+    },
+    main_text: {
+        color: '#000000',
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: 'left',
+    },
+    sub_text: {
+        color: mainColor,
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginTop: 60,
+    },
+    info_text: {
+        fontSize: 20,
+        marginTop: 50,
+    },
+    text_title: {
+        fontSize: 22,
+    },
+    input_text: {
+        fontWeight: 'bold',
+    },
+    text_input: {
+        fontSize: 20,
+        backgroundColor: '#fff',
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderRadius: 6,
+        borderColor: '#000000',
+        height: 70,
+        borderWidth: 1,
+    },
+    login_btn: {
+        marginTop: 70,
+        backgroundColor: '#000000',
+        height: 70,
+        borderRadius: 10,
+        alignItems: 'center',
+    }
+});
+
+export default styles;
