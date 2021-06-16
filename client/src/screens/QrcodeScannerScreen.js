7007fbdb1babb27751afb7a0b5387642464f3300
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-06-17 00:40:54 +0900

    change !!!!

--- client/src/screens/QrcodeScannerScreen.js
+++ client/src/screens/QrcodeScannerScreen.js
@@ -41,7 +41,7 @@ export default class QrcodeScannerScreen extends Component {
   };
 
   _handleBarCodeRead = result => {
-    if (result.data === 'http://563cefae3f78.ngrok.io/qrcode/pointing') {
+    if (result.data === 'http://ec2-34-227-38-106.compute-1.amazonaws.com/qrcode/pointing') {
       if (result.data !== this.state.lastScannedUrl) {
         this.setState({ lastScannedUrl: result.data });
   
